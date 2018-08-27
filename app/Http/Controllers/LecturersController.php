476aa9a4b07f539c9f51f738fafc1efe36f828ed
Varuni-Punchihewa <1995varuni@gmail.com>
2018-08-27 21:53:22 +0530

    return the view of lecturers

--- app/Http/Controllers/LecturersController.php
+++ app/Http/Controllers/LecturersController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Lecturer;
 
 class LecturersController extends Controller
 {
-    //
+    public function index() {
+        $lecturers = Lecturer::all();
+        return view('lecturers');
+    }
 }
