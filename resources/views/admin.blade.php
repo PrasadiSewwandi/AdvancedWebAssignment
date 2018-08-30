01062f034238debbeb804d22aeebf3ae6cc91b75
Sharmilan Thavarajah <sharmilan1994@gmail.com>
2018-08-30 12:12:26 +0530

    User Role Management

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">LECTURER</div>
+
+                <div class="card-body">
+                    
+
+                    This is the lecturer page!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
